45e21ba2d03528c567ae110086f580287dcd5776
Yaël TRAMIER <tramier.yael@gmail.com>
2024-01-23 17:16:04 +0100

    [FIX] vulpy-ssl exercice 3

--- ex_3/vulpy-ssl.py
+++ ex_3/vulpy-ssl.py
@@ -10,7 +10,7 @@ from mod_mfa import mod_mfa
 import libsession
 
 app = Flask('vulpy')
-os.environ['SECRET_KEY'] = 'aaaaaaa'
+app.config['SECRET_KEY'] = os.environ["SECRET_kEY"]
 
 app.register_blueprint(mod_hello, url_prefix='/hello')
 app.register_blueprint(mod_user, url_prefix='/user')
